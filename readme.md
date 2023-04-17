e46b5ea1a5e29573a2c8561e5e8cda8e156421c8
Mac <mac@3sidedcube.com>
2023-04-17 12:39:16 +0100

    feature: sqlite3 example database added

--- readme.md
+++ readme.md
@@ -5,7 +5,14 @@
 - TailwindCSS
 - ESLint
 - Prettier
+- SQLite3
 
 ## Routes
 
 By default the template includes routes for `Home`, `About`, `Get Started` and an API route that returns `{message: "Hello World}`.
+
+## Database
+
+There is an example database file setup to be used with `better-sqlite3`, you can see an example of fetching users from the database in `src/lib/server` and that function is utlised on the `routes/+page.svelte`.
+
+Make sure to remove the example `src/lib/data.db` file before starting work on your amazing new project! This will ensure you have a fresh database to work with.
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,35 @@
-<h1>About</h1>
+<script>
+  export let data;
+
+  const {users} = data;
+</script>
+
+<div class="container">
+  <h1>About</h1>
+  <p>
+    This template uses a <b>SQLite3 Database</b>, this allows for super lean websites with minimal
+    server space taken up, no need for a frontend and backend server it all works seamlessly in a
+    nice sleek package.
+  </p>
+  <p>
+    I'd recommend <b>backing up</b> your SQLite3 Database every once in a while though just to ensure
+    you never lose your data 😁
+  </p>
+</div>
+
+<div class="container">
+  <h2>Users</h2>
+  <p>
+    Here you can see a collection of users pulled from the aforementioned database. Make sure to
+    remove and recreate the <code>src/lib/data.db</code>
+    file before starting work on the new application so that you can begin with a fresh database.
+  </p>
+  <ul>
+    {#each users as user}
+      <li class="flex items-center space-x-2">
+        <h3 class="m-0">{user.name}</h3>
+        <span>{user.bio}</span>
+      </li>
+    {/each}
+  </ul>
+</div>
